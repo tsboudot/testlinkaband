80d34e4331cca4f23b56f2c07e55b42ae05ef0e8
tsboudot <tsboudot@gmail.com>
2024-04-19 15:03:27 +0200

    V1 badge généré

--- src/Badge.jsx
+++ src/Badge.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import artistesData from './variables.json';
 
 const Badge = ({ selectedArtistId }) => {
+    const [generatedBadge, setGeneratedBadge] = useState('');
+
     const generateBadge = () => {
-        if (selectedArtistId) {
-            // Implémentez votre logique de génération de badge ici, en utilisant selectedArtistId
-            console.log(`Génération du badge pour l'artiste ${selectedArtistId}`);
-        } else {
-            console.log('Aucun artiste sélectionné');
+        if (!selectedArtistId) {
+            alert('Veuillez sélectionner un artiste.');
+            return;
         }
+
+        // Récupérer les informations sur l'artiste sélectionné
+        const artiste = artistesData.artistes.find((artiste) => artiste.id === parseInt(selectedArtistId));
+
+        // Générer l'attribut alt
+        const alt = generateAlt(artiste);
+
+        // Générer le code HTML du badge
+        const badgeHtml = `<a href="#" alt="${alt}"><img src="votre-image.jpg" alt="${alt}"></a>`;
+
+        // Mettre à jour l'état avec le badge généré
+        setGeneratedBadge(badgeHtml);
+    };
+
+    const generateAlt = (artiste) => {
+        const randomFirstWord = getRandomFirstWord();
+        const secondWord = Object.values(artiste.styles).some(style => style.name === 'STYLE.DJ') ? 'DJ' : (artiste.nbmembres > 2 ? 'groupe de musique' : 'musicien');
+        const thirdWord = Math.random() < 0.5 ? artiste.localisation : Object.values(artiste.styles).map(style => style.name).join(', ');
+
+        return `${randomFirstWord} ${secondWord} ${thirdWord}`;
+    };
+
+    const getRandomFirstWord = () => {
+        const firstWords = ['Recherche', 'Devis', 'Tarifs', 'Prix', 'Reservation', 'Cherche'];
+        return firstWords[Math.floor(Math.random() * firstWords.length)];
     };
 
     return (
         <div className="badge-container">
             <div>
                 <button onClick={generateBadge}>Générer le badge</button>
+            </div>
+            <div>
+                {/* Affichage du badge généré */}
+                {generatedBadge && <pre>{generatedBadge}</pre>}
             </div>
         </div>
     );
