418cbb7202fe254456a37ca66bbda115b73c9e0e
tsboudot <tsboudot@gmail.com>
2024-04-19 16:17:14 +0200

    Commit Fonctionnel

--- src/Badge.jsx
+++ src/Badge.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'; // Importez useState depuis React
 import artistesData from './variables.json';
 
 const Badge = ({ selectedArtistId }) => {
+    //Hook useState pour mettre à jour la valeur de badge
     const [generatedBadge, setGeneratedBadge] = useState('');
 
     const generateBadge = () => {
@@ -14,16 +15,16 @@ const Badge = ({ selectedArtistId }) => {
         // Récupérer les informations sur l'artiste sélectionné
         const artiste = artistesData.artistes.find((artiste) => artiste.id === parseInt(selectedArtistId));
 
-        // Générer l'attribut alt
+        // Appel fonction alt
         const alt = generateAlt(artiste);
 
         // URL de l'image
         const imageUrl = 'https://images.lesindesradios.fr/fit-in/1100x2000/filters:format(webp)/medias/Vsj0LZpM34/image/Slash_Londres_20221697615214906-format16by9.png';
 
-        // Générer le code HTML du badge avec l'URL de l'image et l'attribut alt
+        // Badge final = String avec le lien, l'URL de l'image, et alt. 
         const badgeHtml = `<!--Badge--><a href="https://linkaband.com" target'_blank'><img src="${imageUrl}" alt="${alt}" style="width: 140px; height: 140px;"/></a>`;
 
-        // Mettre à jour l'état avec le badge généré
+        // Utilisation du Hook pour mettre à jour la valeur de Badge
         setGeneratedBadge(badgeHtml);
     };
 
